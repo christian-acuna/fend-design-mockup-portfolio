936a10ec1bd97cf207868a0f3a360496cd3da88b
Christian Acuna <cacuna0828@gmail.com>
2016-09-16 08:56:17 +0800

    add responsive images and del to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,70 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var cleanCSS = require('gulp-clean-css');
-var plumber = require('gulp-plumber');
+var gulp         = require('gulp');
+var rename       = require('gulp-rename');
+var cleanCSS     = require('gulp-clean-css');
+var postcss      = require('gulp-postcss');
+var sourcemaps   = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer');
+var responsive = require('gulp-responsive');
+var csslint = require('gulp-csslint');
+csslint.addFormatter('csslint-stylish');
+var del = require('del');
+
 var bs = require('browser-sync').create();
+
+var $ = require('gulp-load-plugins')();
+
+gulp.task('images', ['clean:images'], function() {
+  return gulp.src('images_src/*.{jpg,png}')
+    .pipe($.responsive({
+      // Convert all images to JPEG format
+      '*': [{
+        width: 1000,
+        rename: {
+          extname: '.jpg',
+        },
+      }, {
+        // Produce 2x images and rename them
+        width: 1000 * 2,
+        rename: {
+          suffix: '@2x',
+          extname: '.jpg',
+        },
+      }],
+    }))
+    .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('clean:images', function() {
+  return del(['dist/images/**/*']);
+});
 
 gulp.task('scripts', function() {
   gulp.src('src/js/*.js')
-      .pipe(plumber())
-      .pipe(uglify())
-      .pipe(rename('app.min.js'))
+      .pipe($.plumber())
+      .pipe($.uglify())
+      .pipe($.rename('app.min.js'))
       .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('css', function() {
+  gulp.src('src/css/style.css')
+    .pipe(csslint({
+    'shorthand': false
+  }))
+  .pipe(csslint.formatter('stylish'));
+});
+
+gulp.task('autoprefixer', function() {
+  return gulp.src('src/*.css')
+    // .pipe(sourcemaps.init())
+    .pipe(postcss([autoprefixer({browsers: ['last 2 versions']})]))
+    // .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('minify-css', function() {
   return gulp.src('src/css/*.css')
-    .pipe(plumber())
+    .pipe($.plumber())
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('dist/css'))
     .pipe(bs.stream());
@@ -42,4 +91,4 @@ gulp.task('browser-sync', function() {
   });
 });
 
-gulp.task('default', ['scripts', 'minify-css', 'serve']);
+gulp.task('default', ['scripts', 'css', 'minify-css', 'serve']);
