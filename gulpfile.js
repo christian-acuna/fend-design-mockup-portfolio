506ad6443cf4fb9a34b57e3dc4d78760332a77bd
Christian Acuna <cacuna0828@gmail.com>
2016-09-17 11:46:45 +0800

    add art direction for cheetah image

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
-var gulp         = require('gulp');
-var rename       = require('gulp-rename');
-var cleanCSS     = require('gulp-clean-css');
-var postcss      = require('gulp-postcss');
-var sourcemaps   = require('gulp-sourcemaps');
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var cleanCSS = require('gulp-clean-css');
+var postcss = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('autoprefixer');
 var responsive = require('gulp-responsive');
 var csslint = require('gulp-csslint');
-csslint.addFormatter('csslint-stylish');
 var del = require('del');
+csslint.addFormatter('csslint-stylish');
 
 var bs = require('browser-sync').create();
 
@@ -107,24 +107,26 @@ gulp.task('clean:images', function() {
 
 gulp.task('scripts', function() {
   gulp.src('src/js/*.js')
-      .pipe($.plumber())
-      .pipe($.uglify())
-      .pipe($.rename('app.min.js'))
-      .pipe(gulp.dest('dist/js'));
+    .pipe($.plumber())
+    .pipe($.uglify())
+    .pipe($.rename('app.min.js'))
+    .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('css', function() {
   gulp.src('src/css/style.css')
     .pipe(csslint({
-    'shorthand': false
-  }))
-  .pipe(csslint.formatter('stylish'));
+      'shorthand': false
+    }))
+    .pipe(csslint.formatter('stylish'));
 });
 
 gulp.task('autoprefixer', function() {
   return gulp.src('src/*.css')
     // .pipe(sourcemaps.init())
-    .pipe(postcss([autoprefixer({browsers: ['last 2 versions']})]))
+    .pipe(postcss([autoprefixer({
+      browsers: ['last 2 versions']
+    })]))
     // .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist/css'));
 });
@@ -132,12 +134,14 @@ gulp.task('autoprefixer', function() {
 gulp.task('minify-css', function() {
   return gulp.src('src/css/*.css')
     .pipe($.plumber())
-    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(cleanCSS({
+      compatibility: 'ie8'
+    }))
     .pipe(gulp.dest('dist/css'))
     .pipe(bs.stream());
 });
 
-gulp.task('serve',['minify-css'], function() {
+gulp.task('serve', ['minify-css'], function() {
 
   bs.init({
     server: {
